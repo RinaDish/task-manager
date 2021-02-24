b0cdf4be691cb6aa8985a54a4fd18d7cae2b6057
Rina Dishli <dishli271113@gmail.com>
2021-02-24 19:05:07 +0200

    tested nodemailer

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+require("dotenv").config();
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+mongoose.connect(process.env.MONGO_CONNECT_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
-
-
 
 // const smb = new User({
 //   name: "   John",
