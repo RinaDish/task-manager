d9ee9957c3b55dade783d387a48526fbfb20a78e
Rina Dishli <dishli271113@gmail.com>
2021-03-03 20:28:22 +0200

    added cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('./db/mongoose');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 const readmeRouter = require('./routers/readme');
@@ -20,7 +21,12 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 const app = express();
-
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://dish-task-manager.herokuapp.com'],
+    credentials: true,
+  }),
+);
 app.get('/', async (req, res) => res.redirect('/api-docs'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
